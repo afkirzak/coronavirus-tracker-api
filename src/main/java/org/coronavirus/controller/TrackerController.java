8071367a0395f3df97fbfd76b175f3ecfe5feb2e
Zakaria AFKIR <z.afkir.externe@polyexpert.fr>
2020-05-01 23:56:34 +0200

    commit

--- src/main/java/org/coronavirus/controller/TrackerController.java
+++ src/main/java/org/coronavirus/controller/TrackerController.java
@@ -1,8 +1,8 @@
 package org.coronavirus.controller;
 
+import org.coronavirus.model.Data;
 import org.coronavirus.service.TrackerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +18,11 @@ public class TrackerController {
 
     @GetMapping("/welcome")
     public String welcome() {
-        return "Welcome!\nThis is the fast and basic API for tracking development of the new coronavirus (COVID-19)\nV1.2.0";
+        return "Welcome!\nThis is the fast and basic API for tracking development of the new coronavirus (COVID-19)";
     }
 
-    @CrossOrigin
     @GetMapping("/all")
-    public List all() {
+    public List<Data> all() {
         return trackerService.getLastData();
     }
 }
